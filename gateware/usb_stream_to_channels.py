efd57d09cad98914777eb948881687759fbd5207
Hans Baier <hansfbaier@gmail.com>
2021-08-26 05:15:47 +0700

    reimplement usb_stream_to_channels as state machine

--- gateware/usb_stream_to_channels.py
+++ gateware/usb_stream_to_channels.py
@@ -3,7 +3,7 @@ from nmigen.build           import Platform
 from nmigen_library.stream  import StreamInterface
 
 class USBStreamToChannels(Elaboratable):
-    def __init__(self, max_nr_channels):
+    def __init__(self, max_nr_channels=2):
         # parameters
         self._max_nr_channels = max_nr_channels
         self._channel_bits    = Shape.cast(range(max_nr_channels)).width
@@ -12,54 +12,58 @@ class USBStreamToChannels(Elaboratable):
         self.no_channels_in      = Signal(self._channel_bits + 1)
         self.usb_stream_in       = StreamInterface()
         self.channel_stream_out  = StreamInterface(24, extra_fields=[("channel_no", self._channel_bits)])
+        print(str(2**self._channel_bits - 1))
 
     def elaborate(self, platform: Platform) -> Module:
         m = Module()
 
         out_channel_no   = Signal(self._channel_bits)
-        out_byte         = Signal(8)
-        out_byte_counter = Signal(2)
-        out_sample       = Signal(24)
-        usb_valid        = Signal(2)
+        out_sample       = Signal(16)
+        usb_valid        = Signal()
         usb_first        = Signal()
-
-        out_valid = self.channel_stream_out.valid
-
-        m.d.sync += [
-            usb_valid.eq(Cat(self.usb_stream_in.valid, usb_valid)),
-            usb_first.eq(self.usb_stream_in.first),
-            out_valid.eq(0),
-        ]
-
-        with m.If((0 < usb_valid) & self.channel_stream_out.ready):
-            m.d.sync += [
-                out_byte.eq(self.usb_stream_in.payload),
-                out_byte_counter.eq(out_byte_counter + 1),
-            ]
-
-            with m.If(out_byte_counter > 0):
-                m.d.sync +=out_sample.eq(Cat(out_sample[8:], out_byte))
-
-            with m.If((out_byte_counter == 0) & ((usb_valid > 1))):
-                m.d.sync += [
-                    self.channel_stream_out.payload.eq(out_sample),
-                    out_valid.eq(1),
-                    out_channel_no.eq(out_channel_no + 1)
-                ]
-
-        with m.If(self.usb_stream_in.first & self.usb_stream_in.valid):
-            m.d.sync += out_byte_counter.eq(0)
-
-        with m.If(usb_first & usb_valid):
-            m.d.sync += out_channel_no.eq(2**self._channel_bits - 1)
+        usb_payload      = Signal(8)
+        out_ready        = Signal()
 
         m.d.comb += [
-            self.usb_stream_in.ready.eq(self.channel_stream_out.ready),
-            self.channel_stream_out.first.eq(
-                (out_channel_no == 0) & out_valid),
-            self.channel_stream_out.last.eq(
-                (out_channel_no == (self.no_channels_in - 1)) & out_valid),
-            self.channel_stream_out.channel_no.eq(out_channel_no),
+            usb_first.eq(self.usb_stream_in.first),
+            usb_valid.eq(self.usb_stream_in.valid),
+            usb_payload.eq(self.usb_stream_in.payload),
+            out_ready.eq(self.channel_stream_out.ready),
+            self.usb_stream_in.ready.eq(out_ready),
         ]
+
+        m.d.sync += [
+            self.channel_stream_out.valid.eq(0),
+            self.channel_stream_out.first.eq(0),
+            self.channel_stream_out.last.eq(0),
+        ]
+
+        with m.If(usb_valid & out_ready):
+            with m.FSM():
+                with m.State("B0"):
+                    with m.If(usb_first):
+                        m.d.sync += out_channel_no.eq(0)
+                    with m.Else():
+                        m.d.sync += out_channel_no.eq(out_channel_no + 1)
+
+                    m.next = "B1"
+
+                with m.State("B1"):
+                    m.d.sync += out_sample[:8].eq(usb_payload)
+                    m.next = "B2"
+
+                with m.State("B2"):
+                    m.d.sync += out_sample[8:16].eq(usb_payload)
+                    m.next = "B3"
+
+                with m.State("B3"):
+                    m.d.sync += [
+                        self.channel_stream_out.payload.eq(Cat(out_sample, usb_payload)),
+                        self.channel_stream_out.valid.eq(1),
+                        self.channel_stream_out.channel_no.eq(out_channel_no),
+                        self.channel_stream_out.first.eq(out_channel_no == 0),
+                        self.channel_stream_out.last.eq(out_channel_no == (2**self._channel_bits - 1)),
+                    ]
+                    m.next = "B0"
 
         return m
