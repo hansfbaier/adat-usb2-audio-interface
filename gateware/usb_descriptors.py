fda423c0f8620f08483c689196e61b3084439abe
Hans Baier <hansfbaier@gmail.com>
2021-12-09 06:13:36 +0700

    add missing USB descriptors file, and yay, we got our own USB device ID

---
+++ gateware/usb_descriptors.py
@@ -0,0 +1,234 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2021 Hans Baier <hansfbaier@gmail.com>
+# SPDX-License-Identifier: CERN-OHL-W-2.0
+
+from nmigen              import *
+
+from usb_protocol.types                       import USBRequestType, USBRequestRecipient, USBTransferType, USBSynchronizationType, USBUsageType, USBDirection, USBStandardRequests
+from usb_protocol.types.descriptors.uac2      import AudioClassSpecificRequestCodes
+from usb_protocol.emitters                    import DeviceDescriptorCollection
+from usb_protocol.emitters.descriptors        import uac2, standard
+
+class USBDescriptors():
+    def __init__(self, *, max_packet_size, number_of_channels, ila_max_packet_size, use_ila=False) -> None:
+        self.MAX_PACKET_SIZE = max_packet_size
+        self.number_of_channels = number_of_channels
+        self.ILA_MAX_PACKET_SIZE = ila_max_packet_size
+        self.USE_ILA = use_ila
+
+    def create_descriptors(self):
+        """ Creates the descriptors that describe our audio topology. """
+
+        descriptors = DeviceDescriptorCollection()
+
+        with descriptors.DeviceDescriptor() as d:
+            d.bcdUSB             = 2.00
+            d.bDeviceClass       = 0xEF
+            d.bDeviceSubclass    = 0x02
+            d.bDeviceProtocol    = 0x01
+            d.idVendor           = 0x1209
+            d.idProduct          = 0xADA1
+            d.iManufacturer      = "OpenAudioGear"
+            d.iProduct           = "ADATface"
+            d.iSerialNumber      = "0"
+            d.bcdDevice          = 0.01
+
+            d.bNumConfigurations = 1
+
+        with descriptors.ConfigurationDescriptor() as configDescr:
+            # Interface Association
+            interfaceAssociationDescriptor                 = uac2.InterfaceAssociationDescriptorEmitter()
+            interfaceAssociationDescriptor.bInterfaceCount = 3 # Audio Control + Inputs + Outputs
+            configDescr.add_subordinate_descriptor(interfaceAssociationDescriptor)
+
+            # Interface Descriptor (Control)
+            interfaceDescriptor = uac2.StandardAudioControlInterfaceDescriptorEmitter()
+            interfaceDescriptor.bInterfaceNumber = 0
+            configDescr.add_subordinate_descriptor(interfaceDescriptor)
+
+            # AudioControl Interface Descriptor
+            audioControlInterface = self.create_audio_control_interface_descriptor()
+            configDescr.add_subordinate_descriptor(audioControlInterface)
+
+            self.create_output_channels_descriptor(configDescr)
+
+            self.create_input_channels_descriptor(configDescr)
+
+            if self.USE_ILA:
+                with configDescr.InterfaceDescriptor() as i:
+                    i.bInterfaceNumber = 3
+
+                    with i.EndpointDescriptor() as e:
+                        e.bEndpointAddress = USBDirection.IN.to_endpoint_address(3) # EP 3 IN
+                        e.wMaxPacketSize   = self.ILA_MAX_PACKET_SIZE
+
+        return descriptors
+
+
+    def create_audio_control_interface_descriptor(self):
+        audioControlInterface = uac2.ClassSpecificAudioControlInterfaceDescriptorEmitter()
+
+        # AudioControl Interface Descriptor (ClockSource)
+        clockSource = uac2.ClockSourceDescriptorEmitter()
+        clockSource.bClockID     = 1
+        clockSource.bmAttributes = uac2.ClockAttributes.INTERNAL_FIXED_CLOCK
+        clockSource.bmControls   = uac2.ClockFrequencyControl.HOST_READ_ONLY
+        audioControlInterface.add_subordinate_descriptor(clockSource)
+
+
+        # streaming input port from the host to the USB interface
+        inputTerminal               = uac2.InputTerminalDescriptorEmitter()
+        inputTerminal.bTerminalID   = 2
+        inputTerminal.wTerminalType = uac2.USBTerminalTypes.USB_STREAMING
+        # The number of channels needs to be 2 here in order to be recognized
+        # default audio out device by Windows. We provide an alternate
+        # setting with the full channel count, which also references
+        # this terminal ID
+        inputTerminal.bNrChannels   = 2
+        inputTerminal.bCSourceID    = 1
+        audioControlInterface.add_subordinate_descriptor(inputTerminal)
+
+        # audio output port from the USB interface to the outside world
+        outputTerminal               = uac2.OutputTerminalDescriptorEmitter()
+        outputTerminal.bTerminalID   = 3
+        outputTerminal.wTerminalType = uac2.OutputTerminalTypes.SPEAKER
+        outputTerminal.bSourceID     = 2
+        outputTerminal.bCSourceID    = 1
+        audioControlInterface.add_subordinate_descriptor(outputTerminal)
+
+        # audio input port from the outside world to the USB interface
+        inputTerminal               = uac2.InputTerminalDescriptorEmitter()
+        inputTerminal.bTerminalID   = 4
+        inputTerminal.wTerminalType = uac2.InputTerminalTypes.MICROPHONE
+        inputTerminal.bNrChannels   = self.number_of_channels
+        inputTerminal.bCSourceID    = 1
+        audioControlInterface.add_subordinate_descriptor(inputTerminal)
+
+        # audio output port from the USB interface to the host
+        outputTerminal               = uac2.OutputTerminalDescriptorEmitter()
+        outputTerminal.bTerminalID   = 5
+        outputTerminal.wTerminalType = uac2.USBTerminalTypes.USB_STREAMING
+        outputTerminal.bSourceID     = 4
+        outputTerminal.bCSourceID    = 1
+        audioControlInterface.add_subordinate_descriptor(outputTerminal)
+
+        return audioControlInterface
+
+
+    def create_output_streaming_interface(self, c, *, nr_channels, alt_setting_nr):
+        # Interface Descriptor (Streaming, OUT, active setting)
+        activeAudioStreamingInterface                   = uac2.AudioStreamingInterfaceDescriptorEmitter()
+        activeAudioStreamingInterface.bInterfaceNumber  = 1
+        activeAudioStreamingInterface.bAlternateSetting = alt_setting_nr
+        activeAudioStreamingInterface.bNumEndpoints     = 2
+        c.add_subordinate_descriptor(activeAudioStreamingInterface)
+
+        # AudioStreaming Interface Descriptor (General)
+        audioStreamingInterface               = uac2.ClassSpecificAudioStreamingInterfaceDescriptorEmitter()
+        audioStreamingInterface.bTerminalLink = 2
+        audioStreamingInterface.bFormatType   = uac2.FormatTypes.FORMAT_TYPE_I
+        audioStreamingInterface.bmFormats     = uac2.TypeIFormats.PCM
+        audioStreamingInterface.bNrChannels   = nr_channels
+        c.add_subordinate_descriptor(audioStreamingInterface)
+
+        # AudioStreaming Interface Descriptor (Type I)
+        typeIStreamingInterface  = uac2.TypeIFormatTypeDescriptorEmitter()
+        typeIStreamingInterface.bSubslotSize   = 4
+        typeIStreamingInterface.bBitResolution = 24 # we use all 24 bits
+        c.add_subordinate_descriptor(typeIStreamingInterface)
+
+        # Endpoint Descriptor (Audio out)
+        audioOutEndpoint = standard.EndpointDescriptorEmitter()
+        audioOutEndpoint.bEndpointAddress     = USBDirection.OUT.to_endpoint_address(1) # EP 1 OUT
+        audioOutEndpoint.bmAttributes         = USBTransferType.ISOCHRONOUS  | \
+                                                (USBSynchronizationType.ASYNC << 2) | \
+                                                (USBUsageType.DATA << 4)
+        audioOutEndpoint.wMaxPacketSize = self.MAX_PACKET_SIZE
+        audioOutEndpoint.bInterval       = 1
+        c.add_subordinate_descriptor(audioOutEndpoint)
+
+        # AudioControl Endpoint Descriptor
+        audioControlEndpoint = uac2.ClassSpecificAudioStreamingIsochronousAudioDataEndpointDescriptorEmitter()
+        c.add_subordinate_descriptor(audioControlEndpoint)
+
+        # Endpoint Descriptor (Feedback IN)
+        feedbackInEndpoint = standard.EndpointDescriptorEmitter()
+        feedbackInEndpoint.bEndpointAddress  = USBDirection.IN.to_endpoint_address(1) # EP 1 IN
+        feedbackInEndpoint.bmAttributes      = USBTransferType.ISOCHRONOUS  | \
+                                               (USBSynchronizationType.NONE << 2)  | \
+                                               (USBUsageType.FEEDBACK << 4)
+        feedbackInEndpoint.wMaxPacketSize    = 4
+        feedbackInEndpoint.bInterval         = 4
+        c.add_subordinate_descriptor(feedbackInEndpoint)
+
+
+    def create_output_channels_descriptor(self, c):
+        #
+        # Interface Descriptor (Streaming, OUT, quiet setting)
+        #
+        quietAudioStreamingInterface = uac2.AudioStreamingInterfaceDescriptorEmitter()
+        quietAudioStreamingInterface.bInterfaceNumber  = 1
+        quietAudioStreamingInterface.bAlternateSetting = 0
+        c.add_subordinate_descriptor(quietAudioStreamingInterface)
+
+        # we need the default alternate setting to be stereo
+        # out for windows to automatically recognize
+        # and use this audio interface
+        self.create_output_streaming_interface(c, nr_channels=2, alt_setting_nr=1)
+        if self.number_of_channels > 2:
+            self.create_output_streaming_interface(c, nr_channels=self.number_of_channels, alt_setting_nr=2)
+
+
+    def create_input_streaming_interface(self, c, *, nr_channels, alt_setting_nr, channel_config=0):
+        # Interface Descriptor (Streaming, IN, active setting)
+        activeAudioStreamingInterface = uac2.AudioStreamingInterfaceDescriptorEmitter()
+        activeAudioStreamingInterface.bInterfaceNumber  = 2
+        activeAudioStreamingInterface.bAlternateSetting = alt_setting_nr
+        activeAudioStreamingInterface.bNumEndpoints     = 1
+        c.add_subordinate_descriptor(activeAudioStreamingInterface)
+
+        # AudioStreaming Interface Descriptor (General)
+        audioStreamingInterface                 = uac2.ClassSpecificAudioStreamingInterfaceDescriptorEmitter()
+        audioStreamingInterface.bTerminalLink   = 5
+        audioStreamingInterface.bFormatType     = uac2.FormatTypes.FORMAT_TYPE_I
+        audioStreamingInterface.bmFormats       = uac2.TypeIFormats.PCM
+        audioStreamingInterface.bNrChannels     = nr_channels
+        audioStreamingInterface.bmChannelConfig = channel_config
+        c.add_subordinate_descriptor(audioStreamingInterface)
+
+        # AudioStreaming Interface Descriptor (Type I)
+        typeIStreamingInterface  = uac2.TypeIFormatTypeDescriptorEmitter()
+        typeIStreamingInterface.bSubslotSize   = 4
+        typeIStreamingInterface.bBitResolution = 24 # we use all 24 bits
+        c.add_subordinate_descriptor(typeIStreamingInterface)
+
+        # Endpoint Descriptor (Audio out)
+        audioOutEndpoint = standard.EndpointDescriptorEmitter()
+        audioOutEndpoint.bEndpointAddress     = USBDirection.IN.to_endpoint_address(2) # EP 2 IN
+        audioOutEndpoint.bmAttributes         = USBTransferType.ISOCHRONOUS  | \
+                                                (USBSynchronizationType.ASYNC << 2) | \
+                                                (USBUsageType.DATA << 4)
+        audioOutEndpoint.wMaxPacketSize = self.MAX_PACKET_SIZE
+        audioOutEndpoint.bInterval      = 1
+        c.add_subordinate_descriptor(audioOutEndpoint)
+
+        # AudioControl Endpoint Descriptor
+        audioControlEndpoint = uac2.ClassSpecificAudioStreamingIsochronousAudioDataEndpointDescriptorEmitter()
+        c.add_subordinate_descriptor(audioControlEndpoint)
+
+
+    def create_input_channels_descriptor(self, c):
+        #
+        # Interface Descriptor (Streaming, IN, quiet setting)
+        #
+        quietAudioStreamingInterface = uac2.AudioStreamingInterfaceDescriptorEmitter()
+        quietAudioStreamingInterface.bInterfaceNumber  = 2
+        quietAudioStreamingInterface.bAlternateSetting = 0
+        c.add_subordinate_descriptor(quietAudioStreamingInterface)
+
+        # Windows wants a stereo pair as default setting, so let's have it
+        self.create_input_streaming_interface(c, nr_channels=2, alt_setting_nr=1, channel_config=0x3)
+        if self.number_of_channels > 2:
+            self.create_input_streaming_interface(c, nr_channels=self.number_of_channels, alt_setting_nr=2, channel_config=0x0)
+
