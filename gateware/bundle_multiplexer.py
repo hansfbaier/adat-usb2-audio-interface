c07095fbdfb56b3b0fe72fddf5be38ef58a1264b
Hans Baier <hansfbaier@gmail.com>
2021-12-15 10:41:39 +0700

    bundle_multiplexer: allow for inactive bundles (ie ADAT cable not plugged in)

--- gateware/bundle_multiplexer.py
+++ gateware/bundle_multiplexer.py
@@ -1,6 +1,3 @@
-import operator
-from functools import reduce
-
 from nmigen                 import *
 from nmigen.build           import Platform
 from nmigen.lib.fifo        import SyncFIFO
@@ -26,6 +23,10 @@ class BundleMultiplexer(Elaboratable):
                                                          payload_width=self.SAMPLE_WIDTH,
                                                          extra_fields=[("channel_nr", self._bundle_channel_bits)])
                                          for i in range(no_bundles))
+
+        self.bundle_active_in    = Array(Signal(name=f"bundle{i}_active") for i in range(no_bundles))
+
+        self.no_channels_in      = Array(Signal(self._bundle_channel_bits + 1, name=f"no_channels{i}") for i in range(no_bundles))
 
     def elaborate(self, platform: Platform) -> Module:
         m = Module()
@@ -57,29 +58,54 @@ class BundleMultiplexer(Elaboratable):
 
         current_bundle       = Signal(range(self._no_bundles))
         first_bundle_channel = Signal(self._channel_bits)
+        current_channel      = Signal(self._channel_bits)
+        last_bundle          = Signal(self._bundle_channel_bits)
 
-        with m.If(bundle_ready[current_bundle] & self.channel_stream_out.ready):
-            m.d.comb += [
-                self.channel_stream_out.payload.eq(bundle_sample[current_bundle]),
-                self.channel_stream_out.channel_nr.eq(first_bundle_channel + bundle_channel[current_bundle]),
-                read_enable[current_bundle].eq(1),
-                self.channel_stream_out.valid.eq(1),
-                self.channel_stream_out.first.eq((current_bundle == 0) & (bundle_channel[current_bundle] == 0))
-            ]
+        m.d.comb += last_bundle.eq(current_bundle == (self._no_bundles - 1))
 
-            with m.If(last[current_bundle]):
-                last_bundle = (self._no_bundles - 1)
-                with m.If(current_bundle == last_bundle):
-                    m.d.comb += self.channel_stream_out.last.eq(1)
-                    m.d.sync += [
-                        current_bundle.eq(0),
-                        first_bundle_channel.eq(0),
-                    ]
-                with m.Else():
-                    m.d.sync += [
-                        current_bundle.eq(current_bundle + 1),
-                        first_bundle_channel.eq(first_bundle_channel + self.NO_CHANNELS_ADAT)
-                    ]
+        def handle_last_channel():
+            with m.If(last_bundle):
+                m.d.comb += self.channel_stream_out.last.eq(1)
+                m.d.sync += [
+                    current_bundle.eq(0),
+                    first_bundle_channel.eq(0),
+                ]
+            with m.Else():
+                m.d.sync += [
+                    current_bundle.eq(current_bundle + 1),
+                    first_bundle_channel.eq(first_bundle_channel + self.no_channels_in[current_bundle])
+                ]
+
+        with m.If(self.channel_stream_out.ready):
+            # bundle is active (ie. ADAT cable plugged in and synced)
+            with m.If(bundle_ready[current_bundle] & self.bundle_active_in[current_bundle]):
+                m.d.comb += [
+                    self.channel_stream_out.payload.eq(bundle_sample[current_bundle]),
+                    self.channel_stream_out.channel_nr.eq(first_bundle_channel + bundle_channel[current_bundle]),
+                    read_enable[current_bundle].eq(1),
+                    self.channel_stream_out.valid.eq(1),
+                    self.channel_stream_out.first.eq((current_bundle == 0) & (bundle_channel[current_bundle] == 0))
+                ]
+
+                with m.If(last[current_bundle]):
+                    handle_last_channel()
+
+            # bundle inactive (eg. no ADAT/SPDIF cable plugged in or not synced) => fill zeros
+            with m.Elif(~self.bundle_active_in[current_bundle]):
+                last_channel = Signal()
+                m.d.comb += [
+                    self.channel_stream_out.payload.eq(0),
+                    self.channel_stream_out.channel_nr.eq(first_bundle_channel + current_channel),
+                    self.channel_stream_out.valid.eq(1),
+                    self.channel_stream_out.first.eq(0),
+                    self.channel_stream_out.last.eq(0),
+                    last_channel.eq(current_channel == (self.no_channels_in[current_bundle] - 1))
+                ]
+                m.d.sync += current_channel.eq(current_channel + 1)
+
+                with m.If(last_channel):
+                    m.d.sync += current_channel.eq(0)
+                    handle_last_channel()
 
         return m
 
@@ -107,6 +133,9 @@ class BundleMultiplexerTest(GatewareTestCase):
     def test_smoke(self):
         dut = self.dut
         yield
+        for bundle in range(4):
+            yield self.dut.no_channels_in[bundle].eq(8)
+            yield self.dut.bundle_active_in[bundle].eq(1)
         yield dut.channel_stream_out.ready.eq(1)
         yield
         for bundle in range(4):
@@ -114,3 +143,19 @@ class BundleMultiplexerTest(GatewareTestCase):
 
         yield
         yield
+
+    @sync_test_case
+    def test_inactive_bundle(self):
+        dut = self.dut
+        yield
+        yield dut.channel_stream_out.ready.eq(1)
+        for bundle in range(4):
+            yield self.dut.no_channels_in[bundle].eq(8)
+            if (bundle != 2):
+                yield dut.bundle_active_in[bundle].eq(1)
+        yield
+        for bundle in range(4):
+            if (bundle != 2):
+                yield from self.send_bundle_frame(bundle, bundle)
+
+        yield from self.advance_cycles(16)
