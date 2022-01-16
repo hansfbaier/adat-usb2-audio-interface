1a4bebb4c3ad6b48e09d431883ce3803a6a3095f
Hans Baier <hansfbaier@gmail.com>
2022-01-17 06:27:29 +0700

    car: use ResetSynchronizer and factor out multipliers/dividers. Add (unused) 44.1k clocks.

--- gateware/car.py
+++ gateware/car.py
@@ -1,7 +1,17 @@
-from amaranth import *
-from amaranth.build import *
+from amaranth         import *
+from amaranth.build   import *
+from amaranth.lib.cdc import ResetSynchronizer
 
 class IntelFPGAClockDomainGenerator(Elaboratable):
+    ADAT_DIV_48k    = 83
+    ADAT_MULT_48k   = 17
+
+    ADAT_DIV_44_1k  = 62
+    ADAT_MULT_44_1k = 14
+
+    DUTY_CYCLE      = 50
+    NO_PHASE_SHIFT  = 0
+
     def __init__(self, *, clock_frequencies=None, clock_signal_name=None):
         pass
 
@@ -17,9 +27,9 @@ class IntelFPGAClockDomainGenerator(Elaboratable):
 
         clk = platform.request(platform.default_clk)
 
-        sys_clocks   = Signal(1)
-        audio_clocks = Signal(4)
-        fast_clock   = Signal()
+        main_clocks    = Signal(5)
+        audio_clocks   = Signal(4)
+        fast_clock_48k = Signal()
 
         sys_locked   = Signal()
         audio_locked = Signal()
@@ -31,10 +41,33 @@ class IntelFPGAClockDomainGenerator(Elaboratable):
 
             # USB clock: 60MHz
             p_CLK0_DIVIDE_BY         = 5,
-            p_CLK0_DUTY_CYCLE        = 50,
+            p_CLK0_DUTY_CYCLE        = self.DUTY_CYCLE,
             p_CLK0_MULTIPLY_BY       = 6,
-            p_CLK0_PHASE_SHIFT       = 0,
-            p_CLK2_PHASE_SHIFT       = 0,
+            p_CLK0_PHASE_SHIFT       = self.NO_PHASE_SHIFT,
+
+            # 44.1k ADAT Clock 11.2896 MHz = 44.1kHz * 256
+            p_CLK1_DIVIDE_BY         = self.ADAT_DIV_44_1k,
+            p_CLK1_DUTY_CYCLE        = self.DUTY_CYCLE,
+            p_CLK1_MULTIPLY_BY       = self.ADAT_MULT_44_1k,
+            p_CLK1_PHASE_SHIFT       = self.NO_PHASE_SHIFT,
+
+            # I2S DAC clock 44.1k = 3.072 MHz = 44.1kHz * 32 bit * 2 channels
+            p_CLK2_DIVIDE_BY         = self.ADAT_DIV_44_1k * 4,
+            p_CLK2_DUTY_CYCLE        = self.DUTY_CYCLE,
+            p_CLK2_MULTIPLY_BY       = self.ADAT_MULT_44_1k,
+            p_CLK2_PHASE_SHIFT       = self.NO_PHASE_SHIFT,
+
+            # ADAT sampling clock = 44.1kHz * 256 * 8 times oversampling
+            p_CLK3_DIVIDE_BY         = self.ADAT_DIV_44_1k,
+            p_CLK3_DUTY_CYCLE        = self.DUTY_CYCLE,
+            p_CLK3_MULTIPLY_BY       = self.ADAT_MULT_44_1k * 8,
+            p_CLK3_PHASE_SHIFT       = self.NO_PHASE_SHIFT,
+
+            # ADAT transmit domain clock = 44.1kHz * 5 output terminals
+            p_CLK4_DIVIDE_BY         = self.ADAT_DIV_44_1k,
+            p_CLK4_DUTY_CYCLE        = self.DUTY_CYCLE,
+            p_CLK4_MULTIPLY_BY       = self.ADAT_MULT_44_1k * 5,
+            p_CLK4_PHASE_SHIFT       = self.NO_PHASE_SHIFT,
 
             # 50MHz = 20000 picoseconds
             p_INCLK0_INPUT_FREQUENCY = 20000,
@@ -43,7 +76,7 @@ class IntelFPGAClockDomainGenerator(Elaboratable):
             # Drive our clock from the USB clock
             # coming from the USB clock pin of the USB3300
             i_inclk  = clk,
-            o_clk    = sys_clocks,
+            o_clk    = main_clocks,
             o_locked = sys_locked,
         )
 
@@ -51,33 +84,33 @@ class IntelFPGAClockDomainGenerator(Elaboratable):
             p_BANDWIDTH_TYPE         = "AUTO",
 
             # ADAT clock = 12.288 MHz = 48 kHz * 256
-            p_CLK0_DIVIDE_BY         = 83,
-            p_CLK0_DUTY_CYCLE        = 50,
-            p_CLK0_MULTIPLY_BY       = 17,
-            p_CLK0_PHASE_SHIFT       = 0,
+            p_CLK0_DIVIDE_BY         = self.ADAT_DIV_48k,
+            p_CLK0_DUTY_CYCLE        = self.DUTY_CYCLE,
+            p_CLK0_MULTIPLY_BY       = self.ADAT_MULT_48k,
+            p_CLK0_PHASE_SHIFT       = self.NO_PHASE_SHIFT,
 
             # I2S DAC clock 48k = 3.072 MHz = 48 kHz * 32 bit * 2 channels
-            p_CLK1_DIVIDE_BY         = 83 * 4,
-            p_CLK1_DUTY_CYCLE        = 50,
-            p_CLK1_MULTIPLY_BY       = 17,
-            p_CLK1_PHASE_SHIFT       = 0,
+            p_CLK1_DIVIDE_BY         = self.ADAT_DIV_48k * 4,
+            p_CLK1_DUTY_CYCLE        = self.DUTY_CYCLE,
+            p_CLK1_MULTIPLY_BY       = self.ADAT_MULT_48k,
+            p_CLK1_PHASE_SHIFT       = self.NO_PHASE_SHIFT,
 
             # ADAT sampling clock = 48 kHz * 256 * 8 times oversampling
-            p_CLK2_DIVIDE_BY         = 83,
-            p_CLK2_DUTY_CYCLE        = 50,
-            p_CLK2_MULTIPLY_BY       = 17 * 8,
-            p_CLK2_PHASE_SHIFT       = 0,
+            p_CLK2_DIVIDE_BY         = self.ADAT_DIV_48k,
+            p_CLK2_DUTY_CYCLE        = self.DUTY_CYCLE,
+            p_CLK2_MULTIPLY_BY       = self.ADAT_MULT_48k * 8,
+            p_CLK2_PHASE_SHIFT       = self.NO_PHASE_SHIFT,
 
             # ADAT transmit domain clock = 48 kHz * 256 * 5 output terminals
-            p_CLK3_DIVIDE_BY         = 83,
-            p_CLK3_DUTY_CYCLE        = 50,
-            p_CLK3_MULTIPLY_BY       = 17 * 5,
-            p_CLK3_PHASE_SHIFT       = 0,
+            p_CLK3_DIVIDE_BY         = self.ADAT_DIV_48k,
+            p_CLK3_DUTY_CYCLE        = self.DUTY_CYCLE,
+            p_CLK3_MULTIPLY_BY       = self.ADAT_MULT_48k * 5,
+            p_CLK3_PHASE_SHIFT       = self.NO_PHASE_SHIFT,
 
             p_INCLK0_INPUT_FREQUENCY = 16667,
             p_OPERATION_MODE         = "NORMAL",
 
-            i_inclk  = sys_clocks[0],
+            i_inclk  = main_clocks[0],
             o_clk    = audio_clocks,
             o_locked = audio_locked,
         )
@@ -87,31 +120,32 @@ class IntelFPGAClockDomainGenerator(Elaboratable):
 
             # ADAT sampling clock = 48 kHz * 256 * 8 times oversampling
             p_CLK0_DIVIDE_BY         = 1,
-            p_CLK0_DUTY_CYCLE        = 50,
+            p_CLK0_DUTY_CYCLE        = self.DUTY_CYCLE,
             p_CLK0_MULTIPLY_BY       = platform.fast_multiplier,
-            p_CLK0_PHASE_SHIFT       = 0,
+            p_CLK0_PHASE_SHIFT       = self.NO_PHASE_SHIFT,
 
             p_INCLK0_INPUT_FREQUENCY = 81395,
             p_OPERATION_MODE         = "NORMAL",
 
             i_inclk  = audio_clocks[0],
-            o_clk    = fast_clock,
+            o_clk    = fast_clock_48k,
             o_locked = fast_locked,
         )
 
 
         m.d.comb += [
             reset.eq(~(sys_locked & audio_locked & fast_locked)),
-            ClockSignal("fast").eq(fast_clock),
-            ClockSignal("usb") .eq(sys_clocks[0]),
+            ClockSignal("fast").eq(fast_clock_48k),
+            ClockSignal("usb") .eq(main_clocks[0]),
             ClockSignal("sync").eq(audio_clocks[3]),
             ClockSignal("dac").eq(audio_clocks[1]),
             ClockSignal("adat").eq(audio_clocks[0]),
-            ResetSignal("fast").eq(reset),
-            ResetSignal("usb") .eq(reset),
-            ResetSignal("sync").eq(reset),
-            ResetSignal("dac").eq(reset),
-            ResetSignal("adat").eq(reset),
         ]
+
+        m.submodules.reset_sync_fast = ResetSynchronizer(reset, domain="fast")
+        m.submodules.reset_sync_usb  = ResetSynchronizer(reset, domain="usb")
+        m.submodules.reset_sync_sync = ResetSynchronizer(reset, domain="sync")
+        m.submodules.reset_sync_dac  = ResetSynchronizer(reset, domain="dac")
+        m.submodules.reset_sync_adat = ResetSynchronizer(reset, domain="adat")
 
         return m
